7d3b14e4eafa44c46e2bf2e97e94161c371f5aae
thclassixs <thclassixs@gmail.com>
2026-03-06 20:32:51 +0000

    chore: Remove unused `test_xau.js` file.

--- test_xau.js
+++
@@ -1,1 +0,0 @@
-fetch("https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/usd.json").then(res => res.json()).then(data => { console.log("XAU:", data.usd.xau); }).catch(console.error);
